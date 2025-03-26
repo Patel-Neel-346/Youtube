4ff2d0515bf45aaec5f1f229a450a63df43c5f00
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-03-26 21:31:41 +0530

    basic middleware

--- src/app.js
+++ src/app.js
@@ -1,16 +1,39 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/db.js";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 const Port=process.env.PORT || 3000;
 // console.log(process.env.PORT);
-connectDB();
+
+//connect to database
+connectDB().then(()=>{
+    console.log("Database connected successfully!!");
+}).catch((err)=>{
+    console.log(err);
+});
+
+//middleware
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
+app.use(express.json({limit:"30mb",extended:true}));
+app.use(express.urlencoded({limit:"30mb",extended:true}));
+app.use(express.static("public"));
+app.use(cookieParser());    //cookie parser middleware
+
+
+
+//routes
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+
+//listen
 app.listen(Port, () => {
     console.log(`Server is running on port ${Port}`);
 });
