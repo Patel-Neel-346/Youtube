320915e7adbf37f63169ba0da7310bb91e2aad80
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-03-27 22:28:34 +0530

    ApiError && ApiRes Class

---
+++ src/utils/ApiError.js
@@ -0,0 +1,21 @@
+export class ApiError extends Error{
+    constructor(
+        statusCode,
+        message='Internal Server Error',
+        errors=[],
+        stack=""
+    ){
+        super(message);
+        this.statusCode=statusCode;
+        this.errors=errors;
+        this.succes=false;
+        this.message=message;
+        this.data=null;
+
+        if(stack){
+            this.stack=stack;
+        }else{
+            Error.captureStackTrace(this,this.constructor);
+        }
+    }
+}
