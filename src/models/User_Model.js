7cd45fe5c309f5bbb8ddc43f8607d2582a2c3b21
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-03-28 08:08:25 +0530

    Access token && Refresh token && Password Hash && Password Compare

--- src/models/User_Model.js
+++ src/models/User_Model.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 const UserSchema=new mongoose.Schema({
     username:{
@@ -55,6 +58,52 @@ const UserSchema=new mongoose.Schema({
     timestamps:true
 }
 );
+
+//this middleware is used for saving schema it means this middleware run every time where user made any changes into user schema
+
+UserSchema.pre("save",function(next){
+    if(!this.isModified("password")){
+        next();
+    }
+
+    this.password=bcrypt.hash(this.password,10);
+    console.log(this.password);
+    next();
+})
+
+
+//this method is used for comparing password
+UserSchema.methods.isPasswordCorrect=async function(password){
+    return bcrypt.compare(password,this.password);
+}
+
+//this method is used for Refresh generating token
+
+UserSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRES_IN
+    })
+}
+
+//this method is used for Access generating token
+
+UserSchema.methods.generateAccessToken=function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRES_IN
+    })
+}
+
 
 const User=mongoose.model("User",UserSchema);
 
